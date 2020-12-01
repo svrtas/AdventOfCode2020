46c51719babc352f13e5740f551fdf584a39e615
Stella Svrta <stella.svrta@gmail.com>
2020-12-01 16:59:44 +0100

    Solved day1 of AoC

--- AdventOfCode2020-Day1-1/Program.cs
+++ AdventOfCode2020-Day1-1/Program.cs
@@ -1,9 +1,55 @@
-﻿namespace AdventOfCode2020
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace AdventOfCode2020
 {
     internal class Program
     {
+        public static void Part1()
+        {
+            List<int> listOfnumbers = File.ReadAllLines("../../input.txt").Select(int.Parse).ToList();
+
+            /*foreach (var number in listOfnumbers)
+            {
+                if ((number + listOfnumbers.Min()) > 2020 || (number + listOfnumbers.Max()) < 2020)
+                {
+                    listOfnumbers.Remove(number);
+                }
+            }*/
+
+            foreach (var number in listOfnumbers)
+            {
+                foreach (var n in listOfnumbers)
+                {
+                    if((number + n) == 2020) //252 * 1768 : 445536
+                        Console.WriteLine(number + " * " + n + " : " + (number * n));
+                }
+            }
+        }
+        
+        public static void Part2()
+        {
+            List<int> listOfnumbers = File.ReadAllLines("../../input.txt").Select(int.Parse).ToList();
+
+            foreach (var n1 in listOfnumbers)
+            {
+                foreach (var n2 in listOfnumbers)
+                {
+                    foreach (var n3 in listOfnumbers)
+                    {
+                        if((n1+n2+n3) == 2020)
+                            Console.WriteLine(n1 + " * " + n2 + " * " + n3 + " = " + (n1*n2*n3));
+                    }
+                }
+            }
+        }
+        
         public static void Main(string[] args)
         {
+            //Part1();
+            Part2();
         }
     }
 }
