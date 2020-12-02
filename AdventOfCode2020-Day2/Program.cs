dcb03c5759ab9523a1b9f2af44d62815c2ccebbe
Stella Svrta <stella.svrta@gmail.com>
2020-12-02 16:03:28 +0100

    Added project for AoC day 2

---
+++ AdventOfCode2020-Day2/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace AdventOfCode2020_Day2
+{
+    internal class Program
+    {
+        public static void Part1()
+        {
+                
+        }
+        
+        public static void Main(string[] args)
+        {
+            Part1();
+        }
+    }
+}
