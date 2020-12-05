5f59f17899ecf3a3a2b9366d93ce3db78bdec618
Stella Svrta <stella.svrta@gmail.com>
2020-12-05 15:42:54 +0100

    Refactoring

--- AdventOfCode-Day01/Properties/AssemblyInfo.cs
+++ AdventOfCode-Day01/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("AdventOfCode-Day1")]
+[assembly: AssemblyTitle("AdventOfCode-Day01")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("AdventOfCode-Day1")]
+[assembly: AssemblyProduct("AdventOfCode-Day01")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
